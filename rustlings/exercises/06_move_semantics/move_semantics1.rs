29cfdd90f876ec77d418048e3567fb71f112a490
TheOphige <igetheophilus03@gmail.com>
2025-10-16 23:59:55 +0100

    chore

--- rustlings/exercises/06_move_semantics/move_semantics1.rs
+++ rustlings/exercises/06_move_semantics/move_semantics1.rs
@@ -6,6 +6,15 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
     vec
 }
+// TODO: Fix the compiler error in this function.
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let vec = vec;
+
+    vec.push(88);
+
+    vec
+}
+
 
 fn main() {
     // You can optionally experiment here.
